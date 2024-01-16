b09cf9b4a5f7f28a2b802e33b3163093be9a1989
iamedddie <edyolisa@gmail.com>
2024-01-16 14:24:03 +0000

    setup

---
+++ main/java/com/example/UniversityRegistrationSystem/UniversityRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.UniversityRegistrationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ test/java/com/example/UniversityRegistrationSystem/UniversityRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UniversityRegistrationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
