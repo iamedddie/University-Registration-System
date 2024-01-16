837126fc363defda38f51050e137f79a70654c4b
shonanohs <shonafitzpatrick15@gmail.com>
2024-01-16 14:43:01 +0000

    Update schema.sql

--- main/resources/schema.sql
+++ main/resources/schema.sql
@@ -1,56 +1,59 @@
+DROP TABLE IF EXISTS ADDRESS;
+CREATE TABLE ADDRESS (
+     id INT AUTO_INCREMENT  PRIMARY KEY,
+     first_line VARCHAR(255),
+     town VARCHAR(255),
+     postcode VARCHAR(255)
+);
+
 DROP TABLE IF EXISTS STUDENT;
 CREATE TABLE STUDENT (
-     student_id INT AUTO_INCREMENT  PRIMARY KEY,
+     id INT AUTO_INCREMENT  PRIMARY KEY,
      first_name VARCHAR(255),
      last_name VARCHAR(255),
      email VARCHAR(255),
-     major VARCHAR(255),
-     student_year VARCHAR(255),
+     FOREIGN KEY (address_id) REFERENCES ADDRESS (id),
      CHECK (student_year IN ('Freshman', 'Sophomore', 'Junior', 'Senior')),
---      Note:  H2 databases do not handle enums.  When we switch to the MySql database we will be able to use enums
---      We can replace the two lines above with this line:
---      student_year ENUM('Freshman', 'Sophomore', 'Junior', 'Senior'),
      last_registration_date DATE
 );
 
-DROP TABLE IF EXISTS PROFESSOR;
-CREATE TABLE PROFESSOR (
-   professor_id INT AUTO_INCREMENT  PRIMARY KEY,
-   first_name VARCHAR(255),
-   last_name VARCHAR(255),
-   title VARCHAR(255),
-   department VARCHAR(255)
+DROP TABLE IF EXISTS INSTRUCTOR;
+CREATE TABLE INSTRUCTOR (
+   id INT AUTO_INCREMENT  PRIMARY KEY,
+   name VARCHAR(255),
+   email VARCHAR(255),
+   phone_number VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS DEPARTMENT_HEAD;
+CREATE TABLE DEPARTMENT_HEAD (
+   id INT AUTO_INCREMENT  PRIMARY KEY,
+   name VARCHAR(255),
+   email VARCHAR(255),
+   phone_number VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS COURSE;
 CREATE TABLE COURSE (
-    course_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    des VARCHAR(255),
+    pre_reqs VARCHAR(255),
+    term VARCHAR(255),
     name VARCHAR(255),
-    course_number INT,
-    department VARCHAR(255),
-    description TEXT
+    FOREIGN KEY (department_head_id) REFERENCES DEPARTMENT_HEAD (id)
 );
 
-DROP TABLE IF EXISTS COURSE_OFFERING;
-CREATE TABLE COURSE_OFFERING (
-     offering_id INT AUTO_INCREMENT PRIMARY KEY,
-     course_id INT,
-     professor_id INT,
-     start_date DATE,
-     end_date DATE,
-     semester VARCHAR(255)
-
+DROP TABLE IF EXISTS COURSE_CATALOG;
+CREATE TABLE COURSE_CATALOG  (
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    FOREIGN KEY (course_id) references COURSE(id)
 );
 
-DROP TABLE IF EXISTS REGISTRATION;
-CREATE TABLE REGISTRATION (
-      registration_id INT AUTO_INCREMENT PRIMARY KEY,
-      student_id INT,
-      offering_id INT,
-      registration_date DATE,
-      grade VARCHAR(1),
-      CHECK (grade IN ('A', 'B', 'C', 'D', 'F','I', 'W')),
-      class_rating INT,
-      CHECK (class_rating <=5)
-
+DROP TABLE IF EXISTS CLASS;
+CREATE TABLE CLASS  (
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    class_day VARCHAR(250),
+    class_time VARCHAR(250),
+    location VARCHAR(250),
+    FOREIGN KEY (course_id) references COURSE(id), FOREIGN KEY (instructor_id) references INSTRUCTOR(id),
 );
